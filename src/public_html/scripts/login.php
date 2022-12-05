9d4b33b2aac2e07a868a64cafe891f6daeaa50db
Norby99 <72309039+Norby99@users.noreply.github.com>
2022-12-05 17:16:49 +0100

    first script setup

--- src/public_html/scripts/login.php
+++ src/public_html/scripts/login.php
@@ -1,3 +1,22 @@
 <?php
 
+    function isFormValid() {
+        return isset($_POST['email']) && isset($_POST['password']);
+    }
+
+    function main() {
+
+        if (!isFormValid()) {
+            echo("Invalid form");
+            return false;
+        }
+
+    }
+
+    if (main()) {
+        header('Location: /');
+    } else {
+        header('Location: /login.php');
+    }
+
 ?>

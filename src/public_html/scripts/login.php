7fc8fc18bbbc6d07e55e5bf88edc46330bfb3422
Norby99 <72309039+Norby99@users.noreply.github.com>
2022-12-05 17:21:37 +0100

    finished working setup

--- src/public_html/scripts/login.php
+++ src/public_html/scripts/login.php
@@ -4,11 +4,36 @@
         return isset($_POST['email']) && isset($_POST['password']);
     }
 
+    function areUserCredsCorrect($email, $password) {
+        //TODO: Check if the user and password are correct
+    }
+
+    function createSession($email) {
+        //TODO: Create a session for the user
+    }
+
+    function createCookie($email) {
+        //TODO: Create a cookie for the user
+    }
+
     function main() {
         if (!isFormValid()) {
             echo("Invalid form");
             return false;
         }
+
+        if (!areUserCredsCorrect($_POST['email'], $_POST['password'])) {
+            echo("Invalid credentials");
+            return false;
+        }
+
+        createSession($_POST['email']);
+
+        if (isset($_POST['remember'])) {
+            createCookie($_POST['email']);
+        }
+
+        return true;
 
     }
 

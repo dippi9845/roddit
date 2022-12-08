93516bd9ab5247c1014c9875639d3bdeecfc6994
Norby99 <72309039+Norby99@users.noreply.github.com>
2022-12-08 17:35:04 +0100

    added indentation

--- src/public_html/profile/globals.php
+++ src/public_html/profile/globals.php
@@ -1,31 +1,31 @@
 <?php
 
 function areUserCredsCorrect($dbName, $dbUserName, $dbPassword, $userEmail, $userPassword) {
-$conn = new mysqli("localhost", $dbUserName, $dbPassword, $dbName);
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
+    $conn = new mysqli("localhost", $dbUserName, $dbPassword, $dbName);
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
 
-$stmt = $conn->prepare("SELECT Salt FROM users WHERE Email = ?");
-$stmt->bind_param("s", $userEmail);
+    $stmt = $conn->prepare("SELECT Salt FROM users WHERE Email = ?");
+    $stmt->bind_param("s", $userEmail);
 
-$stmt->execute();
-$salt = $stmt->get_result()->fetch_assoc()['Salt'];
+    $stmt->execute();
+    $salt = $stmt->get_result()->fetch_assoc()['Salt'];
 
-$stmt = $conn->prepare("SELECT ID, Email, Password FROM users WHERE Email = ?");
-$stmt->bind_param("s", $userEmail);
-$stmt->execute();
+    $stmt = $conn->prepare("SELECT ID, Email, Password FROM users WHERE Email = ?");
+    $stmt->bind_param("s", $userEmail);
+    $stmt->execute();
 
-$result = $stmt->get_result();
-if ($result->num_rows == 0) {
-    return false;
-}
+    $result = $stmt->get_result();
+    if ($result->num_rows == 0) {
+        return false;
+    }
 
-$hashedPassword = realPass($userPassword, $salt);
+    $hashedPassword = realPass($userPassword, $salt);
 
-$connDbPass = $result->fetch_assoc()['Password'];
+    $connDbPass = $result->fetch_assoc()['Password'];
 
-return password_verify($hashedPassword, $connDbPass);
+    return password_verify($hashedPassword, $connDbPass);
 }
 
 function createSession($email) {

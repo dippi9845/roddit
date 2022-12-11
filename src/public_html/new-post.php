274aa514e25a126ad8ff318054257084f1fa0485
Norby99 <72309039+Norby99@users.noreply.github.com>
2022-12-11 15:22:31 +0100

    check if the user is logged in

--- src/public_html/new-post.php
+++ src/public_html/new-post.php
@@ -1,6 +1,15 @@
 <!DOCTYPE html>
 <html lang="en">
 
+<?php
+include_once($_SERVER['DOCUMENT_ROOT'].'/profile/globals.php');
+session_start();
+
+if (!isUserLoggedIn(true)) {
+    header('Location: /login.php');
+}
+?>
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">

633eb4f9481f52c58d59c7f65f4c4f2231a86857
Norby99 <72309039+Norby99@users.noreply.github.com>
2022-12-12 14:43:48 +0100

    splitted create post in 2 functions

--- src/public_html/profile/create-new-post.php
+++ src/public_html/profile/create-new-post.php
@@ -20,21 +20,24 @@ function saveImage($image) {
     return $path;
 }
 
+function createPostWithFile($conn, $title, $text, $path, $type) {
+    $sql = "INSERT INTO post (Creator, Title, Text, PathToImage, MediaType) VALUES (?, ?, ?, ?, ?)";
+    $stmt = $conn->prepare($sql);
+    if (!$stmt->bind_param("sssss", $_SESSION['userID'], $title, $text, $path, $type)) {
+        return false;
+    }
+    if (!$stmt->execute()) {
+        return false;
+    }
+    $stmt->close();
+    return true;
 }
 
-function createPost($conn, $title, $text, $image=null) {
-    if (!is_null($image)) {
-        $sql = "INSERT INTO posts (Creator, Title, Text, PathToImage, MediaType) VALUES (?, ?, ?, ?, ?)";
-        $stmt = $conn->prepare($sql);
-        if (!$stmt->bind_param("sss", $_SESSION['userId'], $title, $text, $image, $image['type'])) {
-            return false;
-        }
-    } else {
-        $sql = "INSERT INTO posts (Creator, Title, Text) VALUES (?, ?, ?)";
-        $stmt = $conn->prepare($sql);
-        if (!$stmt->bind_param("sss", $_SESSION['userId'], $title, $text)) {
-            return false;
-        }
+function createPost($conn, $title, $text) {
+    $sql = "INSERT INTO post (Creator, Title, Text) VALUES (?, ?, ?)";
+    $stmt = $conn->prepare($sql);
+    if (!$stmt->bind_param("sss", $_SESSION['userID'], $title, $text)) {
+        return false;
     }
 
     if (!$stmt->execute()) {

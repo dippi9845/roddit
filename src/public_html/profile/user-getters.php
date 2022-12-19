670b435e9716ab91cfcd9f9be8689e5255328ec3
Norby99 <72309039+Norby99@users.noreply.github.com>
2022-12-19 15:36:16 +0100

    created follower and following getters

--- src/public_html/profile/user-getters.php
+++ src/public_html/profile/user-getters.php
@@ -7,4 +7,18 @@ function getUserNameByID($conn, $userID) {
     return $row['Nickname'];
 }
 
+function getUserFollowerCount($conn, $userID) {
+    $sql = "SELECT COUNT(*) AS FollowerCount FROM follow WHERE Following = {$userID}";
+    $result = mysqli_query($conn, $sql);
+    $row = mysqli_fetch_assoc($result);
+    return $row['FollowerCount'];
+}
+
+function getUserFollowingCount($conn, $userID) {
+    $sql = "SELECT COUNT(*) AS FollowingCount FROM follow WHERE Follower = {$userID}";
+    $result = mysqli_query($conn, $sql);
+    $row = mysqli_fetch_assoc($result);
+    return $row['FollowingCount'];
+}
+
 ?>

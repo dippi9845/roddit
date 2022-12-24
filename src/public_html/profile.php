344abb17f8410bce0487b3ffa9786285d5874b5f
Norby99 <72309039+Norby99@users.noreply.github.com>
2022-12-24 15:09:27 +0100

    calling un/follow through ajax

--- src/public_html/profile.php
+++ src/public_html/profile.php
@@ -120,6 +120,14 @@ $conn = new mysqli("localhost", $data->dbName, $data->dbPassword, $data->dbUserN
     <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.form/4.3.0/jquery.form.min.js" integrity="sha384-qlmct0AOBiA2VPZkMY3+2WqkHtIQ9lSdAsAn5RUJD/3vA5MKDgSGcdmIv4ycVxyn" crossorigin="anonymous"></script>
 
+    <script>
+        $("#follow-form").ajaxForm({success: function() {
+            location.reload();
+        }});
+        $("#unfollow-form").ajaxForm({success: function() {
+            location.reload();
+        }});
+    </script>
 </body>
 
 </html>

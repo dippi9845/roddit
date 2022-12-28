f2fbf310834419cf45feeb2dd0aee9c2c9e3b4a6
Norby99 <72309039+Norby99@users.noreply.github.com>
2022-12-28 15:09:44 +0100

    converted functions to prepare statement

--- src/public_html/profile/user-getters.php
+++ src/public_html/profile/user-getters.php
@@ -4,44 +4,76 @@ function userExists($conn, $userID) {
     // use prepare statement
     $sql = "SELECT * FROM users WHERE ID = ?";
     $stmt = mysqli_stmt_init($conn);
-    if (mysqli_stmt_prepare($stmt, $sql)) {
-        mysqli_stmt_bind_param($stmt, "i", $userID);
-        mysqli_stmt_execute($stmt);
-        $result = mysqli_stmt_get_result($stmt);
-        if ($row = mysqli_fetch_assoc($result)) {
-            return true;
-        } else {
-            return false;
-        }
+    if (!mysqli_stmt_prepare($stmt, $sql)) {
+        echo("Error description: " . mysqli_error($conn));
+        return;
+    }
+    mysqli_stmt_bind_param($stmt, "i", $userID);
+    mysqli_stmt_execute($stmt);
+    $result = mysqli_stmt_get_result($stmt);
+    if ($row = mysqli_fetch_assoc($result)) {
+        return true;
+    } else {
+        return false;
     }
 }
 
 function getUserNameByID($conn, $userID) {
-    $sql = "SELECT Nickname FROM users WHERE ID = {$userID}";
-    $result = mysqli_query($conn, $sql);
-    $row = mysqli_fetch_assoc($result);
-    return $row['Nickname'];
+    $sql = "SELECT Nickname FROM users WHERE ID = ?";
+    $stmt = mysqli_stmt_init($conn);
+    if (!mysqli_stmt_prepare($stmt, $sql)) {
+        echo("Error description: " . mysqli_error($conn));
+        return;
+    }
+    mysqli_stmt_bind_param($stmt, "i", $userID);
+    mysqli_stmt_execute($stmt);
+    $result = mysqli_stmt_get_result($stmt);
+    if ($row = mysqli_fetch_assoc($result)) {
+        return $row['Nickname'];
+    } else {
+        return false;
+    }
 }
 
 function getUserFollowerCount($conn, $userID) {
-    $sql = "SELECT COUNT(*) AS FollowerCount FROM follow WHERE Following = {$userID}";
-    $result = mysqli_query($conn, $sql);
+    $sql = "SELECT COUNT(*) AS FollowerCount FROM follow WHERE Following = ?";
+    $stmt = mysqli_stmt_init($conn);
+    if (!mysqli_stmt_prepare($stmt, $sql)) {
+        echo("Error description: " . mysqli_error($conn));
+        return;
+    }
+    mysqli_stmt_bind_param($stmt, "i", $userID);
+    mysqli_stmt_execute($stmt);
+    $result = mysqli_stmt_get_result($stmt);
     $row = mysqli_fetch_assoc($result);
     return $row['FollowerCount'];
 }
 
 function getUserFollowingCount($conn, $userID) {
-    $sql = "SELECT COUNT(*) AS FollowingCount FROM follow WHERE Follower = {$userID}";
-    $result = mysqli_query($conn, $sql);
+    $sql = "SELECT COUNT(*) AS FollowingCount FROM follow WHERE Follower = ?";
+    $stmt = mysqli_stmt_init($conn);
+    if (!mysqli_stmt_prepare($stmt, $sql)) {
+        echo("Error description: " . mysqli_error($conn));
+        return;
+    }
+    mysqli_stmt_bind_param($stmt, "i", $userID);
+    mysqli_stmt_execute($stmt);
+    $result = mysqli_stmt_get_result($stmt);
     $row = mysqli_fetch_assoc($result);
     return $row['FollowingCount'];
 }
 
 function isFollowing($conn, $visitedUser, $user) {
-    $sql = "SELECT * FROM follow WHERE Follower = {$user} AND Following = {$visitedUser}";
-    $result = mysqli_query($conn, $sql);
-    $row = mysqli_fetch_assoc($result);
-    if ($row) {
+    $sql = "SELECT * FROM follow WHERE Follower = ? AND Following = ?";
+    $stmt = mysqli_stmt_init($conn);
+    if (!mysqli_stmt_prepare($stmt, $sql)) {
+        echo("Error description: " . mysqli_error($conn));
+        return;
+    }
+    mysqli_stmt_bind_param($stmt, "ii", $user, $visitedUser);
+    mysqli_stmt_execute($stmt);
+    $result = mysqli_stmt_get_result($stmt);
+    if ($row = mysqli_fetch_assoc($result)) {
         return true;
     } else {
         return false;
@@ -52,8 +84,15 @@ function getUserFollowers($conn, $userID) {
     $sql = "SELECT follow.Follower AS ID, users.Nickname
             FROM follow
             INNER JOIN users ON follow.Follower = users.ID
-            WHERE Following = {$userID};";
-    $result = mysqli_query($conn, $sql);
+            WHERE Following = ?;";
+    $stmt = mysqli_stmt_init($conn);
+    if (!mysqli_stmt_prepare($stmt, $sql)) {
+        echo("Error description: " . mysqli_error($conn));
+        return;
+    }
+    mysqli_stmt_bind_param($stmt, "i", $userID);
+    mysqli_stmt_execute($stmt);
+    $result = mysqli_stmt_get_result($stmt);
     $followers = array();
     while ($row = mysqli_fetch_assoc($result)) {
         array_push($followers, $row);
@@ -65,8 +104,15 @@ function getFollowingUsers($conn, $userID) {
     $sql = "SELECT follow.Following AS ID, users.Nickname
             FROM follow
             INNER JOIN users ON follow.Following = users.ID
-            WHERE Follower = {$userID};";
-    $result = mysqli_query($conn, $sql);
+            WHERE Follower = ?;";
+    $stmt = mysqli_stmt_init($conn);
+    if (!mysqli_stmt_prepare($stmt, $sql)) {
+        echo("Error description: " . mysqli_error($conn));
+        return;
+    }
+    mysqli_stmt_bind_param($stmt, "i", $userID);
+    mysqli_stmt_execute($stmt);
+    $result = mysqli_stmt_get_result($stmt);
     $following = array();
     while ($row = mysqli_fetch_assoc($result)) {
         array_push($following, $row);

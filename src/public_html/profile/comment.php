d110490782c49a60bd8fcbf931ab79fca73ba04e
Filippo Di Pietro <filippodp9845@gmail.com>
2023-01-03 06:08:58 +0100

    Update comment.php
    removed useless echo

--- src/public_html/profile/comment.php
+++ src/public_html/profile/comment.php
@@ -3,37 +3,30 @@ include_once $_SERVER['DOCUMENT_ROOT'].'/notify.php';
 
 function comment_post($conn, $userID, $postID, $comment) {
     $sql = "INSERT INTO comments (User, entityType, Comment, entityID) VALUES ('{$userID}', 'Post', '{$comment}', '{$postID}')";
-    if (mysqli_query($conn, $sql)) {
-        echo "Commented successfully<br>";
-    } else {
-        echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    if ( !mysqli_query($conn, $sql) ) {
+        return false;
     }
 
     notify_user($conn, $_POST['userID'], "New comment", "You have a new comment to a post!");
 
     $sql = "UPDATE post SET Comments = Comments + 1 WHERE ID = '{$postID}'";
-    if (mysqli_query($conn, $sql)) {
-        echo "Updated successfully<br>";
-    } else {
-        echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    if ( ! mysqli_query($conn, $sql) ) {
+        return false;
     }
 }
 
 function comment_comment($conn, $userID, $postID, $commentID, $comment) {
     $sql = "INSERT INTO comments (User, entityType, Comment, entityID) VALUES ('{$userID}', 'Commento', '{$comment}', '{$commentID}')";
-    if (mysqli_query($conn, $sql)) {
-        echo "Commented successfully<br>";
-    } else {
-        echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    
+    if ( !mysqli_query($conn, $sql) ) {
+        return false;
     }
 
     notify_user($conn, $_POST['userID'], "New comment", "You have a new comment to your comment!");
 
     $sql = "UPDATE post SET Comments = Comments + 1 WHERE ID = '{$postID}'";
-    if (mysqli_query($conn, $sql)) {
-        echo "Updated successfully<br>";
-    } else {
-        echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    if (! mysqli_query($conn, $sql)) {
+        return false;
     }
 
 }

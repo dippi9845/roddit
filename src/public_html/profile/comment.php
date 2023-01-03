59171dc043d3265c9b60e566efa66724b4215d55
Filippo Di Pietro <filippodp9845@gmail.com>
2023-01-03 05:58:10 +0100

    Update comment.php
    implemented comment

--- src/public_html/profile/comment.php
+++ src/public_html/profile/comment.php
@@ -2,7 +2,7 @@
 include_once $_SERVER['DOCUMENT_ROOT'].'/notify.php';
 
 function comment_post($conn, $userID, $postID, $comment) {
-    $sql = "INSERT INTO comments (User, Post, Comment) VALUES ('{$userID}', '{$postID}', '{$comment}')";
+    $sql = "INSERT INTO comments (User, entityType, Comment, entityID) VALUES ('{$userID}', 'Post', '{$comment}', '{$postID}')";
     if (mysqli_query($conn, $sql)) {
         echo "Commented successfully<br>";
     } else {
@@ -17,8 +17,21 @@ function comment_post($conn, $userID, $postID, $comment) {
     }
 }
 
-function comment_comment() {
-    // TODO
+function comment_comment($conn, $userID, $postID, $commentID, $comment) {
+    $sql = "INSERT INTO comments (User, entityType, Comment, entityID) VALUES ('{$userID}', 'Commento', '{$comment}', '{$commentID}')";
+    if (mysqli_query($conn, $sql)) {
+        echo "Commented successfully<br>";
+    } else {
+        echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    }
+
+    $sql = "UPDATE post SET Comments = Comments + 1 WHERE ID = '{$postID}'";
+    if (mysqli_query($conn, $sql)) {
+        echo "Updated successfully<br>";
+    } else {
+        echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    }
+
 }
 
 ?>

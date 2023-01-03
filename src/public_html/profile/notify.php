be3575249efadefb1d47790b31ffc5da216b066d
Filippo Di Pietro <filippodp9845@gmail.com>
2023-01-03 16:21:00 +0100

    Update notify.php
    added possibility to delete notifications

--- src/public_html/profile/notify.php
+++ src/public_html/profile/notify.php
@@ -1,19 +1,26 @@
 <?php
 
 function notify_user($conn, $userID, $title, $message) {
-    $stmt = $conn->prepare("INSERT INTO notifications (UserID, Title, Message) VALUES (?, ?, ?)");
+    $stmt = $conn->prepare("INSERT INTO notification (UserID, Title, Message) VALUES (?, ?, ?)");
     $stmt->bind_param("sss", $userID, $title, $message);
     $stmt->execute();
     $stmt->close();
 }
 
 function get_notifications($conn, $userID) {
-    $stmt = $conn->prepare("SELECT * FROM notifications WHERE UserID = ?");
+    $stmt = $conn->prepare("SELECT * FROM notification WHERE UserID = ?");
     $stmt->bind_param("s", $userID);
     $stmt->execute();
     $result = $stmt->get_result();
     $stmt->close();
     return $result;
+}
+
+function delete_notification($conn, $userID, $start, $offset) {
+    $stmt = $conn->prepare("DELETE FROM notification WHERE UserID = ? LIMIT ?, ? ORDER BY Inserimento DESC");
+    $stmt->bind_param("sii", $userID, $start, $offset);
+    $stmt->execute();
+    $stmt->close();
 }
 
 ?>

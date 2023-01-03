7371b8c7ac603962938a76a2851fc8fe610d30d1
Filippo Di Pietro <filippodp9845@gmail.com>
2023-01-03 16:29:27 +0100

    Update comment.php
    typo

--- src/public_html/profile/comment.php
+++ src/public_html/profile/comment.php
@@ -2,7 +2,7 @@
 include_once $_SERVER['DOCUMENT_ROOT'].'/notify.php';
 
 function comment_post($conn, $userID, $postID, $comment) {
-    $sql = "INSERT INTO comments (User, entityType, Comment, entityID) VALUES ('{$userID}', 'Post', '{$comment}', '{$postID}')";
+    $sql = "INSERT INTO comment (User, entityType, Comment, entityID) VALUES ('{$userID}', 'Post', '{$comment}', '{$postID}')";
     if ( !mysqli_query($conn, $sql) ) {
         return false;
     }
@@ -16,7 +16,7 @@ function comment_post($conn, $userID, $postID, $comment) {
 }
 
 function comment_comment($conn, $userID, $postID, $commentID, $comment) {
-    $sql = "INSERT INTO comments (User, entityType, Comment, entityID) VALUES ('{$userID}', 'Commento', '{$comment}', '{$commentID}')";
+    $sql = "INSERT INTO comment (User, entityType, Comment, entityID) VALUES ('{$userID}', 'Commento', '{$comment}', '{$commentID}')";
     
     if ( !mysqli_query($conn, $sql) ) {
         return false;

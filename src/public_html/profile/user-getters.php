c592f3750f8b3b9a7e07ab2767517a6a1647b7fe
Norby99 <72309039+Norby99@users.noreply.github.com>
2023-01-04 14:45:37 +0100

    created profile picture getter

--- src/public_html/profile/user-getters.php
+++ src/public_html/profile/user-getters.php
@@ -35,6 +35,23 @@ function getUserNameByID($conn, $userID) {
     }
 }
 
+function getUserProfilePicture($conn, $userID) {
+    $sql = "SELECT ProfileImagePath FROM users WHERE ID = ?";
+    $stmt = mysqli_stmt_init($conn);
+    if (!mysqli_stmt_prepare($stmt, $sql)) {
+        echo("Error description: " . mysqli_error($conn));
+        return;
+    }
+    mysqli_stmt_bind_param($stmt, "i", $userID);
+    mysqli_stmt_execute($stmt);
+    $result = mysqli_stmt_get_result($stmt);
+    if ($row = mysqli_fetch_assoc($result)) {
+        return $row['ProfileImagePath'];
+    } else {
+        return false;
+    }
+}
+
 function getUserFollowerCount($conn, $userID) {
     $sql = "SELECT COUNT(*) AS FollowerCount FROM follow WHERE Following = ?";
     $stmt = mysqli_stmt_init($conn);

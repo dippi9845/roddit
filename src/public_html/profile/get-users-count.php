038bee9ba28277df44dbe222610ce967519de7d2
Norby99 <72309039+Norby99@users.noreply.github.com>
2023-01-06 15:36:51 +0100

    Create get-users-count.php

---
+++ src/public_html/profile/get-users-count.php
@@ -0,0 +1,28 @@
+<?php
+
+include_once($_SERVER['DOCUMENT_ROOT'].'/profile/globals.php');
+include_once($_SERVER['DOCUMENT_ROOT'] . '/html-snippets/user.php');
+
+if (!isUserLoggedIn(true)) {
+    header('Location: /login.php');
+}
+
+$file = file_get_contents($_SERVER['DOCUMENT_ROOT'].'/../setup.json');
+$data = json_decode($file, false);
+
+$conn = new mysqli("localhost", $data->dbName, $data->dbPassword, $data->dbUserName);
+
+if (!isset($_POST['query'])) {
+    $_POST['query'] = "";
+}
+
+$usersCount = 0;
+
+if ($_POST["query"] != "") {
+    $usersCount = getSearchedUsersCount($conn, $_POST["query"]);
+}
+
+echo $usersCount;
+
+$conn->close();
+?>

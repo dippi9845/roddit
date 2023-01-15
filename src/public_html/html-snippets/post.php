28f64f3b6b77b1a0b16cc46f24ad74e3d27bf903
Filippo Di Pietro <filippodp9845@gmail.com>
2023-01-15 15:19:49 +0100

    Update post.php
    bottone per i commenti ?

--- src/public_html/html-snippets/post.php
+++ src/public_html/html-snippets/post.php
@@ -52,10 +52,14 @@ function drawPost($postID, $creatorID, $creatorName, $creatorProfilePicture, $ti
                         </button>
                         </form>
                     <?php } ?>
-                    <a class="card-link" href="#">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chat-fill" viewBox="0 0 16 16">
-                        <path d="M8 15c4.418 0 8-3.134 8-7s-3.582-7-8-7-8 3.134-8 7c0 1.76.743 3.37 1.97 4.6-.097 1.016-.417 2.13-.771 2.966-.079.186.074.394.273.362 2.256-.37 3.597-.938 4.18-1.234A9.06 9.06 0 0 0 8 15z"/>
-                        </svg></a>
+                    <form id="post-like-form-<?= $postID ?>" action="profile/like-post.php" method="post">
+                            <input type="hidden" name="postID" value="<?= $postID ?>">
+                            <button type="submit" class="btn btn-light btn-ajax-form">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chat-fill" viewBox="0 0 16 16">
+                            <path d="M8 15c4.418 0 8-3.134 8-7s-3.582-7-8-7-8 3.134-8 7c0 1.76.743 3.37 1.97 4.6-.097 1.016-.417 2.13-.771 2.966-.079.186.074.394.273.362 2.256-.37 3.597-.938 4.18-1.234A9.06 9.06 0 0 0 8 15z"/>
+                            </svg><?= " ".$comments ?>
+                        </button>
+                    </form>
                 </div>
             </div>
         </div>

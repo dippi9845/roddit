8f8fd458e3fa7b2e048545c22dbbbd8424d8d5a2
Filippo Di Pietro <filippodp9845@gmail.com>
2023-01-15 17:01:23 +0100

    Update settings.php
    better view

--- src/public_html/settings.php
+++ src/public_html/settings.php
@@ -118,6 +118,7 @@ if (file_exists($_FILES['new-photo']['tmp_name']) && is_uploaded_file($_FILES['n
     <title>Roddit</title>
     <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato">
+    <link rel="stylesheet" href="assets/css/settings.css"> <!-- importarlo in questa dir, rinominarlo -->
 </head>
 
 <body>
@@ -127,31 +128,46 @@ if (file_exists($_FILES['new-photo']['tmp_name']) && is_uploaded_file($_FILES['n
                 <h1 class="text-center">Settings</h1>
             </div>
         </div>
-        <?php if ($err) { ?>
         <div class="row">
             <div class="col">
-                <div class="alert alert-danger text-center" role="alert"><span><?= $text_err ?></span></div>
+                <div class="alert alert-danger text-center" role="alert" style="display: none;"><span></span></div>
             </div>
         </div>
-        <?php } ?>
         <div class="row">
-            <div class="col-4">
-            <img>
-            </div>
-            <div class="col">
+            <div class="col-md-6"><img></div>
+            <div class="col-md-6">
                 <form>
-                    <label class="form-label">Change email</label>
-                    <input class="form-control" type="email" placeholder="New Email" name="new-email">
-                    <label class="form-label">Change Nickname</label>
-                    <input class="form-control" type="text" placeholder="New nickname" name="new-nickname">
-                    <label class="form-label">Change Biography</label>
-                    <textarea class="form-control" placeholder="Biography" name="new-biography"></textarea>
-                    <label class="form-label">Change password</label>
-                    <input class="form-control" type="password" name="new-pass" placeholder="New Password">
-                    <input class="form-control" type="password" name="confirm-new-pass" placeholder="Confirm new password">
-                    <label class="form-label">Change Profile image</label>
-                    <input class="form-control" type="file" name="new-photo">
-                    <button class="btn btn-primary" type="submit">Update</button>
+                    <div class="row setting-row">
+                        <div class="col">
+                            <label class="form-label">Change email</label>
+                            <input class="form-control" type="email" placeholder="New Email" name="new-email">
+                        </div>
+                    </div>
+                    <div class="row setting-row">
+                        <div class="col">
+                            <label class="form-label">Change Nickname</label>
+                            <input class="form-control" type="text" placeholder="New nickname" name="new-nickname">
+                        </div>
+                    </div>
+                    <div class="row setting-row">
+                        <div class="col">
+                            <label class="form-label">Change Biography</label>
+                            <textarea class="form-control" placeholder="Biography"></textarea>
+                        </div>
+                    </div>
+                    <div class="row setting-row">
+                        <div class="col">
+                            <label class="form-label">Change password</label>
+                            <input class="form-control" type="password" name="new-pass" placeholder="New Password">
+                            <input class="form-control" type="password" name="confirm-new-pass" placeholder="Confirm new password" style="margin-top: 20px;">
+                        </div>
+                    </div>
+                    <div class="row setting-row">
+                        <div class="col">
+                            <label class="form-label">Change Profile image</label>
+                            <input class="form-control" type="file">
+                        </div>
+                    </div><button class="btn btn-primary" type="button" style="margin-top: 20px;">Update</button>
                 </form>
             </div>
         </div>

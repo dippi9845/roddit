49d3a6e62110d2a2a13f80a1fcff9ba3240c1902
Filippo Di Pietro <filippodp9845@gmail.com>
2023-01-24 10:50:34 +0100

    Update get-my-notification.php
    now gives insert date

--- src/public_html/ajax/get-my-notification.php
+++ src/public_html/ajax/get-my-notification.php
@@ -14,7 +14,7 @@ if (isset($_SESSION['userID'])) {
     $data = json_decode(file_get_contents($_SERVER['DOCUMENT_ROOT'].'/../setup.json'));
     $conn = new mysqli("localhost", $data->dbName, $data->dbPassword, $data->dbUserName);
 
-    $stmt = $conn->prepare("SELECT ID, Title, Message FROM `notification` WHERE `UserID` = ? ORDER BY inserimento DESC LIMIT ?, ?");
+    $stmt = $conn->prepare("SELECT Title, Message, Inserimento FROM `notification` WHERE `UserID` = ? ORDER BY inserimento DESC LIMIT ?, ?");
     $stmt->bind_param("iii", $_SESSION['userID'], $_GET['o'], $_GET['n']);
     $stmt->execute();
     $result = $stmt->get_result();

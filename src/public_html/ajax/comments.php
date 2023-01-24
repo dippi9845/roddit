6473e4c02d9decb81e126213b33fb540bf3a2e3a
Filippo Di Pietro <filippodp9845@gmail.com>
2023-01-24 17:57:20 +0100

    fixed not showing ajax comments

--- src/public_html/ajax/comments.php
+++ src/public_html/ajax/comments.php
@@ -1,31 +1,32 @@
 <?php
-    header('Content-Type: application/json');
+header('Content-Type: application/json');
 
-    $data = json_decode(file_get_contents($_SERVER['DOCUMENT_ROOT'].'/../setup.json'), true);
-    $conn = new mysqli("localhost", $data->dbName, $data->dbPassword, $data->dbUserName);
+$data = json_decode(file_get_contents($_SERVER['DOCUMENT_ROOT'].'/../setup.json'));
+$conn = new mysqli("localhost", $data->dbName, $data->dbPassword, $data->dbUserName);
 
-    if ($conn->connect_error) {
-        die("{ \"Error\": \"" . $conn->connect_error . "\" }");
-    }
+if ($conn->connect_error) {
+    die("{ \"Error\": \"" . $conn->connect_error . "\" }");
+}
 
-    if (!isset($_POST['postID'])) {
-        die("{ \"Error\": \"No ID\" }");
-    }
+if (!isset($_GET['post'])) {
+    die("{ \"Error\": \"No ID\" }");
+}
 
-    $postID = $_POST['postID'];
+$postID = $_GET['post'];
 
-    $stmt = $conn->prepare(
-    "SELECT comment.User, comment.Text, users.ProfileImagePath as ProfileImage
-    FROM comment
-    INNER JOIN users ON comment.User = users.Nickname
-    WHERE `entityType`='Post' AND `entityID`=?
-    ");
+$stmt = $conn->prepare(
+"SELECT comment.User, comment.Text, users.ProfileImagePath as ProfileImage
+FROM comment
+INNER JOIN users ON comment.User = users.Nickname
+WHERE `entityType`='Post' AND `entityID`=?
+");
 
-    $comments = array();
+$stmt->bind_param("i", $postID);
+$stmt->execute();
+$result = $stmt->get_result();
+$comments = $result->fetch_all(MYSQLI_ASSOC);
 
-    while ($row = $result->fetch_assoc()) {
-        $comments[] = $row;
-    }
-
-    echo json_encode($comments);
+$stmt->close();
+$conn->close();
+echo json_encode($comments);
 ?>
--- src/public_html/assets/js/comments-ajax.js
+++ src/public_html/assets/js/comments-ajax.js
@@ -7,13 +7,13 @@ function getComments(postId) {
         type: 'GET',
         data: {post: postId},
         dataType: 'json',
-        success: function (data) {
-            let comments = data.comments;
+        async: false,
+        success: function (comments) {
             let html = '';
             for (var i = 0; i < comments.length; i++) {
                 html += '<div class="d-flex align-items-center">';
                 html += '<div class="flex-shrink-0">';
-                html += '<img class="mr-3" src="' + comments[i].ProfileImage + '" alt="Profile Image">';
+                html += '<img class="mr-3" style="max-width: 64px;" src="' + comments[i].ProfileImage + '" alt="Profile Image">';
                 html += '</div>';
                 html += '<div class="flex-grow-1 ms-3">';
                 html += '<h5 class="mt-0">' + comments[i].User + '</h5>';

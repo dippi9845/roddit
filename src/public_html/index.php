a67758b05ce0327f7898ce6d0c53fb58df9d9a1e
Filippo Di Pietro <filippodp9845@gmail.com>
2023-01-24 11:18:21 +0100

    Update index.php
    added modal for comments

--- src/public_html/index.php
+++ src/public_html/index.php
@@ -78,6 +78,10 @@ $conn = new mysqli("localhost", $data->dbName, $data->dbPassword, $data->dbUserN
     <div class="container" id="posts-container">
         
     </div>
+    <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
+    ...
+    </div>
+
     <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
     <script src="assets/bootstrap/js/bootstrap.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.form/4.3.0/jquery.form.min.js" integrity="sha384-qlmct0AOBiA2VPZkMY3+2WqkHtIQ9lSdAsAn5RUJD/3vA5MKDgSGcdmIv4ycVxyn" crossorigin="anonymous"></script>

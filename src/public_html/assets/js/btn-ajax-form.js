1a19c9ccf20408eed6295f281b152e5139a37dee
Norbert Gabos <dont.vip@gmail.com>
2023-01-25 08:34:29 +0100

    added defulat button behaviour

--- src/public_html/assets/js/btn-ajax-form.js
+++ src/public_html/assets/js/btn-ajax-form.js
@@ -30,3 +30,9 @@ $(".btn-ajax-form-like").click(function() {
         }
     });
 });
+
+$(".btn-ajax-form").click(function() {
+    $("#" + $(this).closest("form").attr('id')).ajaxForm({success: function() {
+        location.reload();
+    }});
+});

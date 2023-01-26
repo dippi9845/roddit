68740ecae46e0dfd16d617b05d9f099523e1dfa8
Filippo Di Pietro <filippodp9845@gmail.com>
2023-01-27 00:17:32 +0100

    Update comments-ajax.js
    added counter

--- src/public_html/assets/js/comments-ajax.js
+++ src/public_html/assets/js/comments-ajax.js
@@ -1,4 +1,5 @@
 let modal = new bootstrap.Modal("#modal-comment");
+let currentCommentButton = null;
 
 function getComments(postId) {
     
@@ -24,6 +25,8 @@ function getComments(postId) {
             $('#modal-comment-body > div').html(html);
         }
     });
+    currentCommentButton = $('#button-section-' + postId + ' > button');
+
     $('#post-id-for-comment').val(postId);
     modal.show();
 }
@@ -53,6 +56,10 @@ $('#send-comment').on('click', function (e) {
             html += '</div>';
             html += '</div>';
             elem.html(html);
+            let num = Number(currentCommentButton.text());
+            let commentImage = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chat-fill" viewBox="0 0 16 16"><path d="M8 15c4.418 0 8-3.134 8-7s-3.582-7-8-7-8 3.134-8 7c0 1.76.743 3.37 1.97 4.6-.097 1.016-.417 2.13-.771 2.966-.079.186.074.394.273.362 2.256-.37 3.597-.938 4.18-1.234A9.06 9.06 0 0 0 8 15z"></path></svg>';
+            
+            currentCommentButton.html(commentImage + " " + String(num + 1));
             $('#comment-textarea').val('');
         }
     });

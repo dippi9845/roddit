97a12865afceb5e9c5d73a69a7c4287e71d5a7f1
Filippo Di Pietro <filippodp9845@gmail.com>
2023-01-27 14:58:24 +0100

    Update noti-ajax.js
    working notifications

--- src/public_html/assets/js/noti-ajax.js
+++ src/public_html/assets/js/noti-ajax.js
@@ -1,4 +1,32 @@
+function setCookie(cname, cvalue, exdays) {
+    const d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    let expires = "expires="+ d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+    let name = cname + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(';');
+    for(let i = 0; i <ca.length; i++) {
+      let c = ca[i];
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return "";
+}
+
 let latest = null;
+
+if (getCookie('latest') != "") {
+    latest = Number(getCookie('latest'));
+}
+
 let showed = 0;
 let badge = false;
 $('#noti-drop > span').hide();
@@ -14,6 +42,7 @@ $('#noti-drop').on('show.bs.dropdown', function () {
     
     if (latest == null) {
         latest = tmp;
+        setCookie('latest', latest, 7);
         nty = show(0);
         console.log(nty);
     }
@@ -24,6 +53,7 @@ $('#noti-drop').on('show.bs.dropdown', function () {
         // and take the difference that must be 
         nty = show(0, tmp - latest);
         latest = tmp;
+        setCookie('latest', latest, 7);
     }
 
     if (nty != null) {
@@ -33,8 +63,6 @@ $('#noti-drop').on('show.bs.dropdown', function () {
     
 });
 
-// TODO: caricamento delle precedenti
-// TODO: salvare il latest nei cookies
 
 setInterval(function () {
     if (latest == null) return;
@@ -47,7 +75,7 @@ setInterval(function () {
 }, 1000);
 
 setInterval(function () {
-    if (notificationList.scrollTop() + notificationList.height() + 20 >= notificationList[0].scrollHeight) {
+    if (notificationList.hasClass("show") && (notificationList.scrollTop() + notificationList.height() + 20 >= notificationList[0].scrollHeight)) {
         console.log('bottom');
         let a = show(showed);
         if (a != null) {

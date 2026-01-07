d87968b5dd1abe3d960b7bd3afb9a7266881a15d
Filippo Di Pietro <filippodp9845@gmail.com>
2026-01-07 12:17:58 +0100

    added function is_user_logged_in

--- src/project/app.py
+++ src/project/app.py
@@ -17,10 +17,82 @@ def index():
     # === Query ===
     query = request.args.get("query", "")
 
-    # Qui puoi caricare i post dal DB
+    # TODO Qui puoi caricare i post dal DB
 
     return render_template("index.html", query=query)
 
+def get_db():
+    pass
+
+def try_login_cookie(conn):
+
+    token = request.cookies.get("login_token")
+    if not token:
+        return False
+
+    pass # TODO check che del cookie nel database
+
+    #TODO check if the cookis is present
+    #if not row:
+    #    return False
+
+    session["userID"] = row["user_id"]
+    return True
+
+def is_user_logged_in(force_cookie_login=False):
+
+    # === Se già in sessione ===
+    if "userID" in session:
+        return True
+
+    # === Se dobbiamo provare con cookie ===
+    if force_cookie_login:
+
+
+        conn = ...
+
+        if try_login_cookie(conn):
+            conn.close()
+            return True
+
+        conn.close()
+
+    return False
+
+
+@app.route("/profile")
+def profile():
+    if not is_user_logged_in(force=True):
+        return redirect("/login")
+
+    conn = get_db()
+
+    visited_user = request.args.get("user", session["userID"])
+
+    if not user_exists(conn, visited_user):
+        return redirect("/404")
+
+    profile_data = {
+        "id": visited_user,
+        "name": get_user_name_by_id(conn, visited_user),
+        "bio": get_user_biography(conn, visited_user),
+        "picture": get_user_profile_picture(conn, visited_user),
+        "followers_count": get_user_follower_count(conn, visited_user),
+        "following_count": get_user_following_count(conn, visited_user),
+        "followers": get_user_followers(conn, visited_user),
+        "following": get_following_users(conn, visited_user),
+        "is_me": visited_user == session["userID"],
+        "is_following": is_following(conn, visited_user, session["userID"])
+    }
+
+    posts = get_users_posts(conn, visited_user)
+
+    for p in posts:
+        p["liked"] = is_liked(conn, p["ID"], session["userID"])
+
+    conn.close()
+
+    return render_template("profile.html", profile=profile_data, posts=posts)
 if __name__ == "__main__":
     app.run(debug=True)
 

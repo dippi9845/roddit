cf34b632ef89a2ae02c61d99d518accc2349a2e3
Filippo Di Pietro <filippodp9845@gmail.com>
2026-01-19 18:45:05 +0100

    implemented get_post_creator in notify.py

--- src/project/notify.py
+++ src/project/notify.py
@@ -2,3 +2,8 @@ from cassandra.cluster import Session as CassandraSession
 
 def notify_user(cs : CassandraSession, user_id, titolo, messaggio):
     cs.execute("INSERT INTO notification (ID, UserID, Titolo, Testo, Inserimento) VALUES (uuid(), ?, ?, ?, toTimestamp(now()))", (user_id, titolo, messaggio,))
+
+def get_post_creator(cs : CassandraSession, post_id):
+    row_post = cs.execute("SELECT Creator FROM post WHERE ID = ?", (post_id,))
+    row_user = cs.execute("SELECT ID FROM users WHERE Nickname = ?", (row_post.Creator,))
+    return row_user.ID

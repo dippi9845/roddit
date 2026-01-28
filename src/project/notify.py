ed9be781b95dd71bb7d368673d7692b350778361
Filippo Di Pietro <filippo.dipietro3@studio.unibo.it>
2026-01-28 16:22:10 +0100

    typo

--- src/project/notify.py
+++ src/project/notify.py
@@ -6,4 +6,4 @@ def notify_user(cs : CassandraSession, user_id, titolo, messaggio):
 def get_post_creator(cs : CassandraSession, post_id):
     row_post = cs.execute("SELECT Creator FROM post WHERE ID = %s", (post_id,))
     row_user = cs.execute("SELECT ID FROM users WHERE Nickname = %s", (row_post[0].creator,))
-    return row_user.ID
+    return row_user[0].id

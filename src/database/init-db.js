39cc3f0f1a2751ec13bd2188c35286b8b62d2b7b
Filippo Di Pietro <filippo.dipietro3@studio.unibo.it>
2026-03-23 19:59:02 +0100

    added sharding indexes into database file

--- src/database/init-db.js
+++ src/database/init-db.js
@@ -1,6 +1,3 @@
-// ============================================================
-// Database setup
-// ============================================================
 db = db.getSiblingDB('roddit');
 
 if (!db.getUser("flask")) {
@@ -11,32 +8,51 @@ if (!db.getUser("flask")) {
     });
 }
 
-// ============================================================
-// Indexes
-// ============================================================
+db.users.dropIndexes();
+db.subreddit.dropIndexes();
+db.following.dropIndexes();
+db.post.dropIndexes();
+db.comment.dropIndexes();
+db.likes.dropIndexes();
+db.notification.dropIndexes();
+db.post_like.dropIndexes();
+db.post_comment.dropIndexes();
+
+
 db.users.createIndex({ "ID": 1 }, { unique: true });
-db.users.createIndex({ "Nickname": 1 }, { unique: true });
-db.users.createIndex({ "Email": 1 }, { unique: true });
-
+db.users.createIndex({ "Nickname": 1 });
+db.users.createIndex({ "Email": 1 });
+ 
 db.subreddit.createIndex({ "Name": 1 }, { unique: true });
-
+ 
 db.following.createIndex({ "User": 1, "Subreddit": 1 }, { unique: true });
-
-db.post.createIndex({ "ID": 1, "Creazione": -1 }, { unique: true });
+ 
+db.post.createIndex({ "ID": 1, "Creazione": -1 });
 db.post.createIndex({ "Subreddit": 1 });
 db.post.createIndex({ "Creator": 1 });
-
-db.comment.createIndex({ "ID": 1 }, { unique: true });
+ 
+db.comment.createIndex({ "ID": 1 });
 db.comment.createIndex({ "entityID": 1 });
-
+ 
 db.likes.createIndex({ "User": 1, "Post": 1 }, { unique: true });
-
-db.notification.createIndex({ "ID": 1, "Inserimento": -1 }, { unique: true });
+ 
+db.notification.createIndex({ "ID": 1, "Inserimento": -1 });
 db.notification.createIndex({ "UserID": 1 });
+ 
+db.post_like.createIndex({ "post": 1 });
+db.post_comment.createIndex({ "post": 1 });
+ 
 
-db.post_like.createIndex({ "post": 1 }, { unique: true });
-db.post_comment.createIndex({ "post": 1 }, { unique: true });
-
+db.users.createIndex({ "ID": "hashed" });
+db.subreddit.createIndex({ "Name": "hashed" });
+db.post.createIndex({ "Subreddit": 1, "Creazione": 1 });
+db.comment.createIndex({ "entityID": "hashed" });
+db.likes.createIndex({ "User": "hashed" });
+db.following.createIndex({ "User": "hashed" });
+db.notification.createIndex({ "UserID": "hashed" });
+db.post_like.createIndex({ "post": "hashed" });
+db.post_comment.createIndex({ "post": "hashed" });
+ 
 
 db = db.getSiblingDB('admin');
 

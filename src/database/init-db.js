a62bcf5fd989404835db7586472bbfba0d6fdeff
Filippo Di Pietro <filippodp9845@gmail.com>
2026-03-26 10:22:57 +0100

    bug fix of not finding the dabase during the initalization

--- src/database/init-db.js
+++ src/database/init-db.js
@@ -1,3 +1,7 @@
+db = db.getSiblingDB('admin');
+
+sh.enableSharding("roddit");
+
 db = db.getSiblingDB('roddit');
 
 if (!db.getUser("flask")) {
@@ -7,6 +11,17 @@ if (!db.getUser("flask")) {
         roles: [{ role: "readWrite", db: "roddit" }]
     });
 }
+
+
+sh.shardCollection("roddit.users", { "ID": "hashed" });
+sh.shardCollection("roddit.subreddit", { "Name": "hashed" });
+sh.shardCollection("roddit.post", { "Subreddit": 1, "Creazione": 1 });
+sh.shardCollection("roddit.comment", { "entityID": "hashed" });
+sh.shardCollection("roddit.likes", { "User": "hashed" });
+sh.shardCollection("roddit.following", { "User": "hashed" });
+sh.shardCollection("roddit.notification", { "UserID": "hashed" });
+sh.shardCollection("roddit.post_like", { "post": "hashed" });
+sh.shardCollection("roddit.post_comment", { "post": "hashed" });
 
 db.users.dropIndexes();
 db.subreddit.dropIndexes();
@@ -22,55 +37,32 @@ db.post_comment.dropIndexes();
 db.users.createIndex({ "ID": 1 }, { unique: true });
 db.users.createIndex({ "Nickname": 1 });
 db.users.createIndex({ "Email": 1 });
- 
+db.users.createIndex({ "ID": "hashed" });
+
 db.subreddit.createIndex({ "Name": 1 }, { unique: true });
- 
+db.subreddit.createIndex({ "Name": "hashed" });
+
 db.following.createIndex({ "User": 1, "Subreddit": 1 }, { unique: true });
- 
+db.following.createIndex({ "User": "hashed" });
+
 db.post.createIndex({ "ID": 1, "Creazione": -1 });
 db.post.createIndex({ "Subreddit": 1 });
 db.post.createIndex({ "Creator": 1 });
- 
+db.post.createIndex({ "Subreddit": 1, "Creazione": 1 });
+
 db.comment.createIndex({ "ID": 1 });
 db.comment.createIndex({ "entityID": 1 });
- 
+db.comment.createIndex({ "entityID": "hashed" });
+
 db.likes.createIndex({ "User": 1, "Post": 1 }, { unique: true });
- 
+db.likes.createIndex({ "User": "hashed" });
+
 db.notification.createIndex({ "ID": 1, "Inserimento": -1 });
 db.notification.createIndex({ "UserID": 1 });
- 
-db.post_like.createIndex({ "post": 1 });
-db.post_comment.createIndex({ "post": 1 });
- 
-
-db.users.createIndex({ "ID": "hashed" });
-db.subreddit.createIndex({ "Name": "hashed" });
-db.post.createIndex({ "Subreddit": 1, "Creazione": 1 });
-db.comment.createIndex({ "entityID": "hashed" });
-db.likes.createIndex({ "User": "hashed" });
-db.following.createIndex({ "User": "hashed" });
 db.notification.createIndex({ "UserID": "hashed" });
+
+db.post_like.createIndex({ "post": 1 });
 db.post_like.createIndex({ "post": "hashed" });
+
+db.post_comment.createIndex({ "post": 1 });
 db.post_comment.createIndex({ "post": "hashed" });
- 
-
-db = db.getSiblingDB('admin');
-
-sh.enableSharding("roddit");
-
-sh.shardCollection("roddit.users", { "ID": "hashed" });
-
-sh.shardCollection("roddit.subreddit", { "Name": "hashed" });
-
-sh.shardCollection("roddit.post", { "Subreddit": 1, "Creazione": 1 });
-
-sh.shardCollection("roddit.comment", { "entityID": "hashed" });
-
-sh.shardCollection("roddit.likes", { "User": "hashed" });
-
-sh.shardCollection("roddit.following", { "User": "hashed" });
-
-sh.shardCollection("roddit.notification", { "UserID": "hashed" });
-
-sh.shardCollection("roddit.post_like", { "post": "hashed" });
-sh.shardCollection("roddit.post_comment", { "post": "hashed" });
